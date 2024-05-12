55121ae5ef809c65c78b7056b6afa57e7cbe344a
Jan Zaydowicz <jan.zaydowicz@railslove.com>
2024-05-12 23:12:12 +0200

    validation of additional rows

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 function checkResult() {
     let form = document.getElementById("database");
     let name = form.elements["name"].checked;
-    let subjects = form.elements["subjects"].checked;
+    let singleSubjects = Array.from(document.querySelectorAll(`td[data-column='subjects']`)).filter(row => row.textContent.includes(',')).length == 0;
 
     if (name) {
         addCorrectClass("name")
     }
 
-    if (subjects) {
+    if (singleSubjects) {
         addCorrectClass("subjects")
     }
 
     let result = document.getElementById("result");
-    userAnswer = name + subjects;
-    result.innerHTML = `${userAnswer} / 4 sind korret`;
+    userAnswer = name + singleSubjects;
+    result.innerText = `${userAnswer} / 4 sind korret`;
 }
 
 function triggerSplitSubjects(element) {
+    console.log('before', currentRow);
     toggleModal('modal-example')
     currentRow = element.parentElement;
+    console.log('after', currentRow);
 }
 
 function splitSubjects() {
-    let subjects = currentRow.children[2].innerHTML.split(", ");
+    console.log(currentRow.children[2]);
+    let subjects = currentRow.children[2].innerText.split(", ");
+    console.log(subjects);
     data = {
-        userID: currentRow.children[0].innerHTML,
-        name: currentRow.children[1].innerHTML,
+        userID: currentRow.children[0].innerText,
+        name: currentRow.children[1].innerText,
         subjects: subjects[1]
     }
     addRow(data);
-    currentRow.children[2].innerHTML = subjects[0];
+    currentRow.children[2].innerText = subjects[0];
     toggleModal('modal-example')
 }
 
@@ -70,6 +74,9 @@ function addRow(data) {
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
     cell1.innerHTML = data.userID;
+    cell3.setAttribute("data-column", "userID");
     cell2.innerHTML = data.name;
+    cell2.setAttribute("data-column", "name");
     cell3.innerHTML = data.subjects;
+    cell3.setAttribute("data-column", "subjects");
 }
