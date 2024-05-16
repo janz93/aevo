bfa17b479dfa0ca741a0e5523ef3c898e50a97e2
Jan Zaydowicz <jan.zaydowicz@railslove.com>
2024-05-16 23:34:55 +0200

    feat: user stories

--- script.js
+++ script.js
@@ -1,40 +1,72 @@
 function checkResult() {
     let form = document.getElementById("database");
-    let name = form.elements["name"].checked;
+    let name = document.querySelector(`[data-column='firstname']`)
     let singleSubjects = Array.from(document.querySelectorAll(`td[data-column='subjects']`)).filter(row => row.textContent.includes(',')).length == 0;
 
     if (name) {
-        addCorrectClass("name")
+        checkAcceptanceCriteria("ac-names");
     }
 
     if (singleSubjects) {
-        addCorrectClass("subjects")
+        checkAcceptanceCriteria("ac-subjects");
     }
-
-    let result = document.getElementById("result");
-    userAnswer = name + singleSubjects;
-    result.innerHTML = `${userAnswer} / 2 Optimierung erkannt`;
 }
 
 function triggerSplitSubjects(element) {
     console.log('before', currentRow);
-    toggleModal('modal-example')
+    toggleModal('model-row-split')
     currentRow = element.parentElement;
     console.log('after', currentRow);
 }
 
 function splitSubjects() {
-    console.log(currentRow.children[2]);
-    let subjects = currentRow.children[2].innerText.split(", ");
-    console.log(subjects);
-    data = {
-        userID: currentRow.children[0].innerText,
-        name: currentRow.children[1].innerText,
-        subjects: subjects[1]
+    let subjects = currentRow.querySelector(`[data-column='subjects']`).innerText.split(', ');
+    hasName = currentRow.querySelector(`[data-column='firstname']`);
+
+    if (hasName) {
+        data = {
+            userID: currentRow.children[0].innerText,
+            firstname: currentRow.querySelector(`[data-column='firstname']`).innerText,
+            name: currentRow.querySelector(`[data-column='name']`).innerText,
+            subjects: subjects[1]
+        }
+        currentRow.children[3].innerText = subjects[0];
+    } else {
+        data = {
+            userID: currentRow.children[0].innerText,
+            name: currentRow.querySelector(`[data-column='name']`).innerText,
+            subjects: subjects[1]
+        }
+        currentRow.children[2].innerText = subjects[0];
     }
+
+    console.log(data);
     addRow(data);
-    currentRow.children[2].innerText = subjects[0];
-    toggleModal('modal-example')
+    toggleModal('model-row-split')
+}
+
+function splitColumnName() {
+    let table = document.getElementById("normalization");
+    let headerRow = table.rows[0];
+    if (headerRow.cells[1].getAttribute("data-column") == "firstname") {
+        return;
+    }
+    // add new th and add the second position
+    let newColumn = document.createElement("th");
+    newColumn.innerHTML = "Vorname";
+    newColumn.setAttribute("data-column", "firstname");
+    headerRow.insertBefore(newColumn, headerRow.cells[1]);
+
+    // Add the new column to the second position for the rest of the rows
+    for (let i = 1; i < table.rows.length; i++) {
+        let row = table.rows[i];
+        let name = row.cells[1].innerText.split(" ");
+        console.log(row.cells[1]);
+        let cell = row.insertCell(1);
+        cell.setAttribute("data-column", "firstname");
+        cell.innerHTML = name[0];
+        row.cells[2].innerText = name[1];
+    }
 }
 
 function filterTable() {
@@ -67,16 +99,35 @@ function addCorrectClass(columnName) {
     });
 }
 
+function checkAcceptanceCriteria(id) {
+    document.getElementById(id).classList.add("correct");
+}
+
 function addRow(data) {
     let table = document.getElementById("normalization");
     let row = table.insertRow(-1);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    cell1.innerHTML = data.userID;
-    cell3.setAttribute("data-column", "userID");
-    cell2.innerHTML = data.name;
-    cell2.setAttribute("data-column", "name");
-    cell3.innerHTML = data.subjects;
-    cell3.setAttribute("data-column", "subjects");
+    if (data.firstname) {
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        cell1.innerHTML = data.userID;
+        cell1.setAttribute("data-column", "userID");
+        cell2.innerHTML = data.firstname;
+        cell2.setAttribute("data-column", "firstname");
+        cell3.innerHTML = data.name;
+        cell3.setAttribute("data-column", "name");
+        cell4.innerHTML = data.subjects;
+        cell4.setAttribute("data-column", "subjects");
+    } else {
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerHTML = data.userID;
+        cell1.setAttribute("data-column", "userID");
+        cell2.innerHTML = data.name;
+        cell2.setAttribute("data-column", "name");
+        cell3.innerHTML = data.subjects;
+        cell3.setAttribute("data-column", "subjects");
+    }
 }
